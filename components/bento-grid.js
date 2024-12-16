552a11021a5d02c48167533221359438d010d372
Faizan Ahmad <fiaznhmd.0624@gmail.com>
2024-12-16 12:21:25 +0530

    feat

--- components/bento-grid.js
+++ components/bento-grid.js
@@ -1,13 +1,12 @@
-"use client";
+'use client';
 
-import { cn } from "../lib/utils";
-
+import { cn } from '../lib/utils';
 
 export const BentoGrid = ({ className, children }) => {
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
+        'grid md:auto-rows-[19rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ',
         className
       )}
     >
@@ -16,11 +15,17 @@ export const BentoGrid = ({ className, children }) => {
   );
 };
 
-export const BentoGridItem = ({ className, title, description, header, icon }) => {
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  header,
+  icon,
+}) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4',
         className
       )}
     >
